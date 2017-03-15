094ff8ce15214a13b48982a282a7adcf671f65c3
WalterBrito <waltter.britto@gmail.com>
2017-03-15 11:44:20 -0300

    Add CSS

--- gallery/settings.py
+++ gallery/settings.py
@@ -124,12 +124,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-# STATIC_ROOT = os.path.join(gallery_app,'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#                 os.path.join(gallery_app,'static'),
-# )
-# TEMPLATE_DIRS = (
-#                 os.path.join(gallery_app,'templates'),
-# )
-MEDIA_ROOT = os.path.join
